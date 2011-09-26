1a70c09f6146b0597ca85725fee22e4ff3df219d
Zeray Rice <fanzeyi1994@gmail.com>
2011-09-26 09:59:57 -0700

    money accepted

--- two/money/money.c
+++ two/money/money.c
@@ -24,11 +24,21 @@ int main(void) {
     FILE *fin = fopen("money.in","r");
     FILE *fout = fopen("money.out", "w");
     int v, n; 
-    int i; 
+    int i, j; 
+    long long dp[10001] = {0}; 
     int money[26]; 
     fscanf(fin, "%d %d", &v, &n);
-    for( i = 0 ; i < n ; i++ ) {
+    for( i = 0 ; i < v ; i++ ) {
         fscanf(fin, "%d", &money[i]);
     }
+    dp[0] = 1;
+    for( i = 0 ; i < v ; i++ ) {
+        for( j = 1 ; j <= n ; j++ ) {
+            if(j - money[i] >= 0) {
+                dp[j] = dp[j] + dp[j-money[i]]; 
+            }
+        }
+    }
+    fprintf(fout, "%lld\n", dp[n]); 
     return 0;
 }
