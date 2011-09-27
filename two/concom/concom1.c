50be0edae38742777baebc46704621fdee6d6805
Zeray Rice <fanzeyi1994@gmail.com>
2011-09-27 04:05:54 -0700

    accepted..

--- two/concom/concom1.c
+++ two/concom/concom1.c
@@ -24,6 +24,9 @@ int stock[101][101]; // i 公司拥有 j 公司的 xx 股份
 int can[101][101];  // i 公司 控制 j 公司
 
 void AddCan(int mother, int son) {
+    if(can[mother][son]) {
+        return; 
+    }
     can[mother][son] = 1; 
     int i; 
     for( i = 0 ; i < 101 ; i++ ) {
@@ -34,9 +37,8 @@ void AddCan(int mother, int son) {
             AddCan(i, son); 
         }
     }
-    
     for( i = 0 ; i < 101 ; i++ ) {
-        if(stock[mother][i] >= 50 && !can[mother][i]) {
+        if(stock[mother][i] > 50) {
             AddCan(mother, i); 
         }
     }
@@ -51,11 +53,10 @@ void AddStock(int mother, int son, int percent) {
         }
     }
     for( i = 0 ; i < 101 ; i++ ) {
-        if(stock[i][son] >= 50 && !can[i][son]) {
+        if(stock[i][son] > 50) {
             AddCan(i, son); 
         }
     }
-    
 }
 
 int main(void) {
@@ -65,10 +66,16 @@ int main(void) {
     int i, j; 
     int a, b, c; 
     fscanf(fin, "%d", &n); 
+    for( i = 0 ; i < 101 ; i++ ) {
+        for( j = 0 ; j < 101 ; j ++ ) {
+            stock[i][j] = 0; 
+            can[i][j] = 0; 
+        }
+        can[i][i] = 1; 
+    }
     for( i = 0 ; i < n ; i++ ) {
         fscanf(fin, "%d %d %d", &a, &b, &c); 
         if(a == b) continue; 
-        stock[a][b] += c; 
         AddStock(a, b, c); 
     }
     for( i = 0 ; i < 101 ; i++ ) {
